051b6d395514bb77bb0280052181ca253299db27
codr4life <codr4life@gmail.com>
2018-10-14 22:58:49 +0200

    added init script

---
+++ dist/init.sl
@@ -0,0 +1,6 @@
+'user
+['id db.new-str-column] []
+db.new-table
+let: user-table
+
+@db @user-table create sync!
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ int main(int argc, const char *argv[]) {
   snabl::Env env;
   env.add_lib<libs::DB>();
   env.use(snabl::Pos(-1, -1), env.sym("s.abc"));
+  env.load("init.sl");
   snabl::repl(env, cin, cout);
   return 0;
 }
