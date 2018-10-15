f3bfeaa2272ecf6a6a02a9d53645656a3340262c
codr4life <codr4life@gmail.com>
2018-10-15 06:14:34 +0200

    added notebook

--- src/snackis/gui/app.cpp
+++ src/snackis/gui/app.cpp
@@ -1,3 +1,5 @@
+#include <wx/notebook.h>
+
 #include "snabl/env.hpp"
 #include "snackis/gui/app.hpp"
 #include "snackis/libs/db.hpp"
@@ -9,10 +11,24 @@ namespace snackis::gui {
     snabl::Env env;
     env.add_lib<libs::DB>();
     env.use(env.sym("s.abc"));
-    //env.load("init.sl");
 
-    wxFrame *f(new wxFrame(nullptr, -1, "Snackis"));
+    auto f(new wxFrame(nullptr, -1, "Snackis"));
     f->Maximize(true);
+
+    auto p(new wxPanel(f));
+    auto ps(new wxNotebook(p, wxID_ANY));
+    auto s(new wxBoxSizer(wxVERTICAL));
+    p->SetSizer(s);
+    s->Add(ps, 1, wxEXPAND);
+
+    ps->AddPage(new wxPanel(ps), "1 Console");
+    ps->AddPage(new wxPanel(ps), "2 Peers");
+    ps->AddPage(new wxPanel(ps), "3 Threads");
+    ps->AddPage(new wxPanel(ps), "4 Settings");
+    ps->AddPage(new wxPanel(ps), "5 In");
+    ps->AddPage(new wxPanel(ps), "6 Out");
+
+    //env.load("init.sl");
     f->Show(true);
     SetTopWindow(f);
     return true;
