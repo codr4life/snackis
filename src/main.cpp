bc3b2e014921af3afaf3a55a6fe91720c3a94479
codr4life <codr4life@gmail.com>
2018-10-22 00:53:45 +0200

    reorg

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "snackis/types/db/context.hpp"
 
 using namespace snackis;
+snabl::Env env;
 
 GtkWidget *new_page (GtkNotebook *parent, const char *icon, const char *title) {
   auto f(gtk_box_new(GTK_ORIENTATION_VERTICAL, 0));
@@ -22,8 +23,24 @@ GtkWidget *new_page (GtkNotebook *parent, const char *icon, const char *title) {
   return f;
 }
 
-static void on_activate(GtkApplication *app, gpointer user_data) {
-  snabl::Env env;
+static void init_gui(GtkApplication *app) {
+  auto w(gtk_application_window_new(app));
+  gtk_window_set_icon_from_file(GTK_WINDOW(w), "images/snackis.ico", nullptr);
+  gtk_window_set_title(GTK_WINDOW(w), "Snackis v0.1.1");
+  gtk_window_maximize(GTK_WINDOW(w));
+
+  auto ps(gtk_notebook_new());
+  new_page(GTK_NOTEBOOK(ps), "console", "_0 Console");
+  new_page(GTK_NOTEBOOK(ps), "in", "_1 In");
+  new_page(GTK_NOTEBOOK(ps), "out", "_2 Out");
+  new_page(GTK_NOTEBOOK(ps), "peers", "_3 Peers");
+  new_page(GTK_NOTEBOOK(ps), "threads", "_4 Threads");
+  new_page(GTK_NOTEBOOK(ps), "settings", "_5 Settings");
+  gtk_container_add(GTK_CONTAINER(w), ps);
+  gtk_widget_show_all(w);
+}
+
+static void init_snabl() {
   auto &db_lib(env.add_lib<libs::DB>());
   env.use(env.sym("s.abc"));
 
@@ -41,29 +58,18 @@ static void on_activate(GtkApplication *app, gpointer user_data) {
           db::ContextPtr::make(&db_lib.context_type.pool, db));
 
   env.load("scripts/init.sl");
-  
-  auto w(gtk_application_window_new(app));
-  gtk_window_set_icon_from_file(GTK_WINDOW(w), "images/snackis.ico", nullptr);
-  gtk_window_set_title(GTK_WINDOW(w), "Snackis v0.1.1");
-  gtk_window_maximize(GTK_WINDOW(w));
+}
 
-  auto ps(gtk_notebook_new());
-  new_page(GTK_NOTEBOOK(ps), "console", "_0 Console");
-  new_page(GTK_NOTEBOOK(ps), "in", "_1 In");
-  new_page(GTK_NOTEBOOK(ps), "out", "_2 Out");
-  new_page(GTK_NOTEBOOK(ps), "peers", "_3 Peers");
-  new_page(GTK_NOTEBOOK(ps), "threads", "_4 Threads");
-  new_page(GTK_NOTEBOOK(ps), "settings", "_5 Settings");
-  gtk_container_add(GTK_CONTAINER(w), ps);
-  gtk_widget_show_all(w);
+static void on_activate(GtkApplication *app, gpointer _) {
+  init_gui(app);
+  init_snabl();
 }
 
 int main(int argc, char *argv[]) {
   GtkApplication *app;
-  int status;
   app = gtk_application_new("codr4life.snackis", G_APPLICATION_FLAGS_NONE);
   g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
-  status = g_application_run(G_APPLICATION(app), argc, argv);
+  auto status(g_application_run(G_APPLICATION(app), argc, argv));
   g_object_unref(app);
   return status;
 }
